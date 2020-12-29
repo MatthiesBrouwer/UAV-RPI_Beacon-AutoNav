6f79c1fd2d83b71ba2f13f8e5bb8458fc722444c
MatthiesBrouwer <ties1000@live.nl>
2020-12-29 15:52:51 +0100

    Rwrote some structures and added the filter  in a clearer way

--- uwb_uav_simulation_ws/src/uwb_localization_description/src/uwb_localization_publisher.py
+++ uwb_uav_simulation_ws/src/uwb_localization_description/src/uwb_localization_publisher.py
@@ -12,42 +12,14 @@ import os, sys
 
 
 
-
-class MedianDistributor:
-	def __init__(self, max_points):
-		self.max_points = max_points
-		self.point_queue = []
-		
-
-	
-	def add_point(self, new_point):
-
-		self.point_queue.append(new_point)
-		if len(self.point_queue) >= self.max_points:
-			self.point_queue.pop(0)
-
-			x = np.arange(1, self.max_points , 1)
-			#plt.plot(x, self.point_queue, linewidth=2, linestyle="-", c="b")
-			n = 5
-			b = [1.0 / n] * n
-			a = 1
-			yy = lfilter(b, a, self.point_queue)
-			#plt.plot(x, yy, linewidth=2, linestyle="-", c="r")
-			#self.calc_weighted_median(yy)
-			#t = np.arange(0.0, self.max_points, 1)
-			#plt.subplot(131)
-			#plt.bar(t, self.queue)
-			#plt.show()
-			print("		CURRENT QUEUE: {}".format(self.point_queue))
-			print("\tCURRENT ESTIMATION:\n\t{}\n\tINITIAL DISTANCE:\n\t{}".format(yy[-1], self.point_queue[-1]))
-
 class TriangulatorNode3D:
 	def __init__(self, point_position_all, filter_index = 50):
 		#--- Declare variables ---#
 		self.uwb_data_raw_sub = rospy.Subscriber("/uwb_data_topic", uwb_data_raw, self.update_callback, queue_size = 10)
 		self.pub = rospy.Publisher("/position_data_uwb", position_data_uwb, queue_size = 20)
 		self.point_position_all = point_position_all
-		self.distance_queue = []
+		self.point_distance_all = np.zeros(len(point_position_all))
+		self.prev_distances = [[0] * len(point_position_all) for i in range(len(point_position_all))]
 		self.filter_index = filter_index
 		self.position_estimation = None
 	#### Function: This  function return the current position estimation
@@ -58,39 +30,29 @@ class TriangulatorNode3D:
 
 	def update_callback(self, uwb_data_raw_msg):
 		#--- Save data obtained from topic to queue and update queue---#
-		self.distance_queue.append(uwb_data_raw_msg.distance)
+		for index, distance in enumerate(uwb_data_raw_msg.distance):
+			self.prev_distances[index].append(distance)
+			if len(self.prev_distances[index]) >= self.filter_index:
+				self.prev_distances[index].pop(0)
 
-
-		if len(self.distance_queue) >= self.filter_index:
-			self.distance_queue.pop(0)
-			distances_first = []
-			for distances in self.distance_queue:
-				distances_first.append(distances[0])
-			#--- Apply noise mitigating filter to current distance estimations --#
-			x = np.arange(1, self.filter_index + 1 , 1)
-			#plt.plot(x, self.point_queue, linewidth=2, linestyle="-", c="b")
-			n = 5
-			b = [1.0 / n] * n
-			a = 1
-			yy = lfilter(b, a, distances_first)
-                        print("\tCURRENT ESTIMATION:\n\t{}\n\tINITIAL DISTANCE:\n\t{}".format(yy[-1], self.distance_queue[-1]))
-
+			#--- Apply noise mitigating filter to current distance estimations ---#
+				x = np.arange(1, self.filter_index + 1, 1)
+				n = 15
+				b = [1.0 / n] * n
+				a = 1
+				yy = lfilter(b, a, self.prev_distances[index])
+	                        distance_holder_TEST = self.prev_distances[index][-1]
+				#self.distance_queues[index][-1] = yy[-1]
+				#print("\tCURRENT ESTIMATION:\n\t{}\n\tINITIAL DISTANCE:\n\t{}\n\n".format(yy[-1], self.distance_queues[index][-1]))
+				print("INDEX {}\n\tCURRENT ESTIMATION:\n\t{}\n\tINITIAL DISTANCE:\n\t{}".format(index, yy[-1], distance_holder_TEST))
+				self.point_distance_all[index] = yy[-1]
 		self.position_estimation = self.triangulate_target_position()
 		self.position_estimation = [self.position_estimation[0]/1000, self.position_estimation[1]/1000, self.position_estimation[2]/1000]
-		print("		NEW TRIANGULATION POSITION: {}, {}, {}".format(self.position_estimation[0] / 100, self.position_estimation[1] / 100, self.position_estimation[2] / 100))
-		print("=================================\n\n")
+		print("\tNEW TRIANGULATION POSITION: {}, {}, {}".format(self.position_estimation[0] / 100, self.position_estimation[1] / 100, self.position_estimation[2] / 100))
+		
 
+		print("====================================\n\n")
 
-
-
-
-		#--- Recalculate measured position and publish result ---#
-		#self.median_distributor.add_point(self.point_distance_all[0])
-		#print("		NEW DISTANCES: {}, {}, {}, {}".format(self.point_distance_all[0],self.point_distance_all[1],self.point_distance_all[2],self.point_distance_all[3]))
-		#self.position_estimation = self.triangulate_target_position()
-		#self.position_estimation = [self.position_estimation[0]/1000, self.position_estimation[1]/1000, self.position_estimation[2]/1000]
-		#print("		NEW TRIANGULATION POSITION: {}, {}, {}".format(self.position_estimation[0] / 100, self.position_estimation[1] / 100, self.position_estimation[2] / 100))
-		#print("=================================\n\n")
 
 
 	def triangulate_target_position(self):
@@ -100,10 +62,10 @@ class TriangulatorNode3D:
                 point_3 = np.array(self.point_position_all[2])
                 point_4 = np.array(self.point_position_all[3])
 
-                radius_1 = int(self.distance_queue[-1][0])
-                radius_2 = int(self.distance_queue[-1][1])
-                radius_3 = int(self.distance_queue[-1][2])
-                radius_4 = int(self.distance_queue[-1][3])
+                radius_1 = int(self.point_distance_all[0])
+                radius_2 = int(self.point_distance_all[1])
+                radius_3 = int(self.point_distance_all[2])
+                radius_4 = int(self.point_distance_all[3])
 
 
 		e_x = (point_2 - point_1) / np.linalg.norm(point_2 - point_1)
